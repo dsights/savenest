031a9126383fa61164c13eb127ff1c3a968f73fc
SaveNest Developer <developer@savenest.app>
2026-02-14 01:30:12 +1100

    fix: add missing imports for kIsWeb and MetaSEO in ComparisonScreen

--- lib/features/comparison/comparison_screen.dart
+++ lib/features/comparison/comparison_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meta_seo/meta_seo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../theme/app_theme.dart';
